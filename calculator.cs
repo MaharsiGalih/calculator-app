d48eef5eac9638fb201bca2142d17393b1d83bb1
MaharsiGalih <maharsigalih@students.amikom.ac.id>
2024-03-06 17:02:12 +0700

    Menambahkan fungsi pengurangan, belum selesai

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,11 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10; 
+		int b = 6;
 		
+		Conselo.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b))
+		Conselo.WriteLine("Hasil Pengurangan:1} = {2}", a, b, Pengurangan(a, b))
 	}
 	
 	static int Penambahan(int a, int b)
